ceeeb9bb5e294c52e5c62d8148f97638d191d869
MustafaYagci <mustafamusabyagci@gmail.com>
2020-08-01 10:49:42 +0200

    Runners&Build

---
+++ src/test/java/com/pccomponentes/runners/Runner.java
@@ -0,0 +1,18 @@
+package com.pccomponentes.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports",
+                "rerun:target/rerun.txt"},
+        features = "src/test/resources/Features",
+        glue ="com/pccomponentes/stepDefs",
+        dryRun = true,
+        tags = "@tag"
+
+)
+public class Runner {
+}
